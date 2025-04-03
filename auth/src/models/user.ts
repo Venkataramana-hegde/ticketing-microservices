82bd31a6b7ed6e8293bd7f7634a0df4dbb13ca90
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-04-03 15:46:58 +0530

    Implement entire authentication features

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -2,51 +2,64 @@ import mongoose from "mongoose";
 import { Password } from "../services/password";
 
 //An interface that describes the properties that are required to create a new user
-interface UserAttrs{
-    email: string;
-    password: string;
+interface UserAttrs {
+  email: string;
+  password: string;
 }
 
 //Am interface that describes the properties that a User model has
-interface UserModel extends mongoose.Model<UserDoc>{
-    build(attrs: UserAttrs): UserDoc;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
 //An interface that describes the properties that a User document has
-interface UserDoc extends mongoose.Document{
-    //Any new properties should be added here
-    email: string;
-    password: string;
+interface UserDoc extends mongoose.Document {
+  //Any new properties should be added here
+  email: string;
+  password: string;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      unique: true
     },
     password: {
-        type: String,
-        required: true
-    }
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
-userSchema.pre('save', async function(done){
-    if(this.isModified('password')){
-        const hashed = await Password.toHash(this.get('password'));
-        this.set('password', hashed);
-    }
-    done();
-})
-
 userSchema.statics.build = (attrs: UserAttrs) => {
-    return new User(attrs);
+  return new User(attrs);
 };
 
 const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 const user = User.build({
-    email: 'test@test.com',
-    password: 'password'
-})
+  email: "test@test.com",
+  password: "password",
+});
 
 export { User };
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,61 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { Password } from "../services/password";
+import { validateRequest } from "../middlewares/validate-requests";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there');
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // console.log("Stored Hashed Password:", existingUser.password); 
+    // console.log("Entered Password:", password); 
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    // console.log("Passwords Match:", passwordsMatch); 
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+    //Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //store it in a session object
+    req.session = {
+      jwt: userJwt,
+    };
+    // console.log("Session at Signup:", req.session); 
+    // console.log("Session at Signin:", req.session); 
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
